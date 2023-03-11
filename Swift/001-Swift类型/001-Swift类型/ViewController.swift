c6b9490ba184359cf2ff0367df6f426923f19459
gaoguangxiao <gaoguangxiao125@sina.com>
2023-03-11 14:26:26 +0800

    基本类型和字符串

---
+++ Swift/001-Swift类型/001-Swift类型/ViewController.swift
@@ -0,0 +1,179 @@
+//
+//  ViewController.swift
+//  001-Swift类型
+//
+//  Created by gaoguangxiao on 2023/2/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //1、类型
+        //        typealias dog = Int
+        //        var a:dog = 12
+        //        var b:Int = 18
+        //        print(a,b)
+        
+        //2、() ?? 类型转换 可选类型
+        
+        //3、有些类型不能设置为nil
+        /*
+         var c:Int = 18
+         c = nil
+         无法编译通过
+         */
+        
+        /*
+         var d:Int
+         print(d)
+         */
+        
+        /*
+         var e :Int?
+         print(e)
+         可编译通过
+         */
+        
+        
+        //4、元组数据 可拆分
+        //        不同类型数据的封装
+        //        var c = ("str",12)
+        //        print(c)
+        
+        //5、穿透 fallthrough 略过进入下一步
+//        let f = 10
+//        switch f {
+//        case 10:
+//            print("1")
+//            fallthrough
+//        case 20:
+//            print("2")
+//        case 30:
+//            print("3")
+//        default:
+//            print(0)
+//        }
+        
+        //6、范围运算符
+//        0...10 0到10
+//        0..<10 0到9
+//        var g = 0...10
+//        var g = 0..<10
+//        for index in g {
+//            print(index)
+//        }
+        
+        //不包括
+//        for index in stride(from: 0, to: 10, by: 2) {
+//            print(index)
+//        }
+//        //包括 through
+//        for index in stride(from: 0, through: 10, by: 2) {
+//            print(index)
+//        }
+        
+        //7、while
+//        var h = 0
+//        while h < 10 {
+//            h+=1
+//            print(h)
+//        }
+//        var g = 0
+//        repeat {
+//            g+=1
+//            print(g)
+//        }while g<10
+        
+        //7、字符串
+        /*
+         var str = "123131"
+ //        str.index(<#T##i: String.Index##String.Index#>, offsetBy: <#T##Int#>)
+ //        str.index(after: <#T##String.Index#>)
+         print(str)
+         
+ //        两字符串比较是否包含同一元素
+         var str1 = "1343535"
+         print(str.contains("1"))
+         
+         print(str.contains(where: String.contains("145678")))
+ //        追加
+         str.append("1313131422")
+         print(str)
+         */
+        
+        /*
+         var dic = ["12":"value"]
+ //        dic.keys
+ //        dic
+ //        dic.values
+         dic.updateValue("ad", forKey: "12")
+         print(dic)
+         */
+        
+        /*//        let a = 10
+         //        assert(a != 10,"a必须等于10")
+         //        print("a = \(a)")
+         */
+        
+//        let a : ()->Void = test
+//        a()
+//        //无参数 无返回值
+//        let a1 : ()->Void = {()->Void in
+//            print("匿名函数")
+//        }
+//        a1()
+
+        
+        //有参无返回值
+//        let b:(String) ->Void = tests(s:)
+//        b("好")
+//
+//        let b1 :(String) -> Void = { (s:String) -> Void in
+//            print("有参无返回值\(s)")
+//        }
+//        b1("好1")
+        
+//        无参有返回值
+//        let c :() -> String = {() -> String in
+//            return "好好"
+//        }
+//
+////        有参有返回值
+//        let d:(String) -> String = { (a:String) -> String in
+//
+//            return a + "adad"
+//        }
+                
+//        print(c())
+//        print(d("4567y8uijokhy7"))
+        
+        
+//        funMe(para: test)
+        
+        sun(para: (a: Int, b: Int))
+    }
+    
+    func test() {
+        print("test1")
+    }
+    
+    func tests(s:String) -> Void {
+        print(s)
+    }
+    
+    func funMe(para:() -> Void) {
+        para()
+    }
+    
+    func sun(para:(Int,Int) -> Int) -> Int {
+    para(Int,Int)
+    }
+    
+    
+}
+
---
+++ Swift/002-swift字符串/002-swift字符串/main.swift
@@ -0,0 +1,78 @@
+//
+//  main.swift
+//  002-swift字符串
+//
+//  Created by gaoguangxiao on 2023/3/11.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var str = "ABCDEF"
+
+var value = """
+ABCDEFG
+  ABCDEFG
+                ABCDEFG
+"""
+
+//打印特殊字符
+value =  #""ABCDE""#
+print(value)
+
+
+//字符串遍历
+//for s in  str {
+//    print("\(s)")
+//}
+//
+//for i in 0..<str.count {
+//    let s = str[str.index(str.startIndex, offsetBy: i)]
+//    print(s)
+//}
+
+
+//print(str.contains("A"))
+
+//print(str.contains(where: String.contains("AL")))
+
+//print(str.hasPrefix("A"))
+
+//字符串替换
+let si = str.index(str.startIndex, offsetBy: 2)
+let se = str.index(str.startIndex, offsetBy: 3)
+str.replaceSubrange(si...se, with: "RR")
+print(str)
+
+//将某字符替换
+//str.replacingOccurrences(of: <#T##StringProtocol#>, with: <#T##StringProtocol#>)
+
+//1、输出
+//print("输出str：\(str)")
+//2、打印长度
+//print("字符串str长度：\(str.count)")
+
+//print(str.endIndex)
+
+//str[] []里面需要的是string.index类型
+//str.index(before: str.endIndex) 返回 string.index
+//3、打印最后一个字
+//let eIndex = str.index(before: str.endIndex)
+//print(str[eIndex])
+
+//4、打印指定偏移
+//let oIndex = str.index(str.startIndex, offsetBy: 2)
+//let o1Index = str.index(str.startIndex, offsetBy: 4)
+////0..2 区间语法
+//print(str[oIndex..<o1Index])
+
+//5、
+//print(str.prefix(4))
+
+//6、查找某个字符出现的位置
+//var index = str.firstIndex(of: "E") ?? str.endIndex
+//print(str[index])
+
+//print(str[str.index(after: str.startIndex)])
+
