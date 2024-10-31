475cb84d74c8c4478cd1289818dc1fc1b7226047
gaoguangxiao <gaoguangxiao125@sina.com>
2024-10-31 18:19:12 +0800

    kvc整理

--- Objective-C/002-OC类/004-KVC实现原理/004-KVC实现原理/LGPerson.m
+++ Objective-C/002-OC类/004-KVC实现原理/004-KVC实现原理/LGPerson.m
@@ -9,4 +9,30 @@
 
 @implementation LGPerson
 
+//- (void)setName:(NSString *)name {
+//    NSLog(@"setName");
+//}
+//
+//- (void)_setName:(NSString *)name {
+//    NSLog(@"_setName");
+//}
+
+//这二步执行的前提是 setName/_setName没有找到
+//+ (BOOL)accessInstanceVariablesDirectly {
+//    return NO;
+//}
+
+//第三步判断成员变量前提是 accessInstanceVariablesDirectly设置为YES
+//_name/_isName/name/isName
+
+//第四步 成员变量找不到 执行此方法
+//- (void)setValue:(id)value forUndefinedKey:(NSString *)key
+//- (void)setValue:(id)value forUndefinedKey:(NSString *)key {
+//
+//}
+
+
+//如果accessInstanceVariables返回NO，就不查找成员变量，直接执行第四步；
+//只要重写第四步方法 就不会抛出异常了？
+
 @end
